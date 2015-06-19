47406b56bffc83dab965769a3a283cc4f1ce2ba6
milind-webonise <milind.chaudhari@weboniselab.com>
2015-06-19 16:33:03 +0530

    db_backup

--- cookbooks/db_backup/recipes/default.rb
+++ cookbooks/db_backup/recipes/default.rb
@@ -1,30 +1,72 @@
 # default recipe
 
 
-include_recipe "db_backup::mysql"
-include_recipe "db_backup::postgres"
+execute "Create directory for sanity check script" do
+command "mkdir -p #{node[:scripts]} "
+end
+
+case node['database']['type']
+
+  when "mysql","MYSQL","Mysql"
+
+	execute "Create backup directory for mysql" do
+	command "mkdir -p #{node[:backup]}mysql"
+	command "mkdir -p #{node[:backup_scripts][:target_directory]} "
+	end
+
+	template "/root/scripts/mysql_backup.sh" do
+	  source "mysql_backup.sh.erb"
+	  owner "root"
+	  mode "0700"
+	end
+
+	cron "schedule sanity check sheet" do
+	  hour node[:backup_scripts][:hour]
+	  minute node[:backup_scripts][:minute]
+	  command " sh /root/scripts/mysql_backup.sh >/var/log/mysql-backup.log 2>&1"
+	end
+
+ ## Scheduling  server sanity chech script
+  
+ 	template "/root/scripts/server_sanity_check.sh" do
+          source "mysql_apache.sh.erb"
+	  owner "root"
+	  mode "0700"
+	 end
+
+   when "Postgres","POSTGRES","postgres"
+   
+    execute "Create backup directory for postgres" do
+	command "mkdir -p #{node[:backup_scripts][:target_directory]} "
+	command "mkdir -p #{node[:backup]}postgres"
+	end
 
 
-#include_recipe "db_backup::mysql"
-#include_recipe "db_backup::postgres"
+	template "/root/scripts/postgres_backup.sh" do
+	  source "postgres_backup.sh.erb"
+	  owner "root"
+	  mode "0700"
+	end
+
+	cron "Schedule Postgres backup" do
+	  hour node[:backup_scripts][:hour]
+	  minute node[:backup_scripts][:minute]
+	  command " sh /root/scripts/postgres_backup.sh >/var/log/postgres-backup.log 2>&1"
+	end
 
 
+#   when "mongo","Mongo","MONGO"
+#
+#        template "/root/scripts/server_sanity_check.sh" do
+#          source "mongo_apache.sh.erb"
+#	  owner "root"
+#	  mode "0700"
+#        end
+	
+#	cron "schedule sanity check script" do
+#        hour node[:script][:hour]
+#        minute node[:script][:minute]
+#        command " sh /root/scripts/server_sanity_check.sh > /var/log/server_sanity.log"
+#        end
 
-
-
-
-
-
-
-
-
-
-
-
-
-#execute "Create backup directory for mysql" do
-#command "mkdir -p #{node[:backup_scripts][:target_directory]} "
-#command "mkdir -p #{node[:backup]}mysql"
-#end
-
-
+end
